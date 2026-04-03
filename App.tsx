bd91e3f2f6e0c95657882e7a3013dbefa6091f97
sohaibnazir65-gif <sohaibnazir65@gmail.com>
2026-04-03 16:36:10 +0500

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import { Switch, Route, Router as WouterRouter } from "wouter";
+import Converter from "./Converter";
+import PrivacyPolicy from "./PrivacyPolicy";
+import ContactUs from "./ContactUs";
+
+export default function App() {
+  return (
+    <WouterRouter>
+      <Switch>
+        <Route path="/" component={Converter} />
+        <Route path="/privacy" component={PrivacyPolicy} />
+        <Route path="/contact" component={ContactUs} />
+        <Route component={Converter} />
+      </Switch>
+    </WouterRouter>
+  );
+}
+
